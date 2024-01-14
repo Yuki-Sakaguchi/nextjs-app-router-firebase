6e4ac49350dd50ac6d4c3fb59708bab23c4662e9
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2024-01-14 22:41:39 +0900

    middleware を追加

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest, response: NextResponse) {
+  const session = request.cookies.get("session");
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+    headers: {
+      Cookie: `session=${session?.value}`,
+    },
+  });
+
+  if (responseAPI.status !== 200) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
